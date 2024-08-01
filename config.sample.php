d121847d9f5161829b8a9a282da5d376be8d241b
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-01 22:00:00 +0600

    add config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'weather_app');
+define('DB_USER', 'root'); // Change if necessary
+define('DB_PASS', ''); // Change if necessary
+
+define('WEATHER_API_KEY', 'your_api_key_here'); // Change if necessary //sample: bd5e378503939ddaee76f12ad7a97608
+define('WEATHER_API_URL', 'http://api.openweathermap.org/data/2.5/weather');
+?>
